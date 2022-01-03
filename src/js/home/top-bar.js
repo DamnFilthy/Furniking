bf441b3fce6f5109af63a96e948f801c5cad4d84
Alex <alexander-shiryaev-1993@mail.ru>
2022-01-03 13:24:50 +0300

    initial commit top-bar added

---
+++ src/js/home/top-bar.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('.js-language-dropdown').on('tap click', function(){
+        $('.js-drop-rotate').toggleClass('drop-svg-rotate');
+        $('.js-language-drop-show').toggleClass('drop-show');
+    })
+})
