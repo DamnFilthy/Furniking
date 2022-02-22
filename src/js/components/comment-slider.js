a179f9677cc52546cd7977d035e48c461efe0bd7
Alex <a.shiryaev@ansaligy.com>
2022-02-22 23:31:25 +0300

    section products ended, comments slider added

---
+++ src/js/components/comment-slider.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.comments-slider').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: false,
+    });
+});
